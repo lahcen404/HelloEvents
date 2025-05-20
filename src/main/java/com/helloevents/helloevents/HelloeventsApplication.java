0f79f00af5dbc3360cff057f3892f1c26e3076a4
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-20 15:20:29 +0100

    Create HelloEvents Project

---
+++ src/main/java/com/helloevents/helloevents/HelloeventsApplication.java
@@ -0,0 +1,13 @@
+package com.helloevents.helloevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloevents/helloevents/HelloeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloevents.helloevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
