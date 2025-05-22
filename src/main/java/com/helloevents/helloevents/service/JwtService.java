4cf163f14d18f53b7241c8285fd6770e4c0ed9f5
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-22 16:00:39 +0100

    Create JwtAthenticationFiler & JwtService

--- src/main/java/com/helloevents/helloevents/service/JwtService.java
+++ src/main/java/com/helloevents/helloevents/service/JwtService.java
@@ -1,10 +1,81 @@
 package com.helloevents.helloevents.service;
 
+import com.helloevents.helloevents.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.security.Signature;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY ="ed83d985e2f00253574b242a2b9ec8d5e5b202024135ff6e6bbfb5e573a585d6";
     public String extractUserEmail(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token , Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignIngKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public boolean isTokenValid(String token , UserDetails userDetails){
+        final String username = extractUserEmail(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSignIngKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignIngKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
