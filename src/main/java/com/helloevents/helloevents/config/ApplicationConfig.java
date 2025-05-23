075ab9fd2710fc59a3b96377f73d42641d270004
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-23 10:34:19 +0100

    Create Security Config

--- src/main/java/com/helloevents/helloevents/config/ApplicationConfig.java
+++ src/main/java/com/helloevents/helloevents/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.helloevents.helloevents.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,4 +22,5 @@ public class ApplicationConfig {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found !!!"));
     }
+
 }
