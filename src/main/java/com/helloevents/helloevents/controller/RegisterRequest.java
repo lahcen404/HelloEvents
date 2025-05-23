539e0b9a217c995ee85ba56f132a60fc9cb406e3
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-23 11:18:35 +0100

    Create AuthRequest - AuthResponse - RegisterRequest Classes

---
+++ src/main/java/com/helloevents/helloevents/controller/RegisterRequest.java
@@ -0,0 +1,8 @@
+package com.helloevents.helloevents.controller;
+
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
