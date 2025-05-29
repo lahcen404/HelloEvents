16f2714e4f14cc43ffe2af9f2d2f1fdcc995e896
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-29 23:05:19 +0100

    Create Footer

--- HelloEvents-FrontEnd/src/app/app.component.ts
+++ HelloEvents-FrontEnd/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import {NavbarComponent} from "./shared/components/navbar/navbar.component";
 import {SidebarComponent} from "./shared/components/sidebar/sidebar.component";
 import {EventListComponent} from "./core/event/event-list/event-list.component";
+import {FooterComponent} from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent, SidebarComponent, EventListComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, SidebarComponent, EventListComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
